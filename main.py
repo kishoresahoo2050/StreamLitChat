1d62070feeba285aada22e357c802e0359f4c1a4
kishore sahoo <mytechtut@mytechtut.onmicrosoft.com>
2026-02-16 23:38:30 +0530

    SteamLit code added.

---
+++ main.py
@@ -0,0 +1,32 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import streamlit as st
+
+#load .evv variable (google api key import)
+load_dotenv()
+
+# Initialize model with gemini
+model = ChatGoogleGenerativeAI(
+    model="models/gemini-2.5-flash",
+    temperature=0.35
+)
+
+# set header and title for page
+st.set_page_config(page_title="Gemini Research Tool!")
+st.header("🔍 Research Tool")
+
+#take user input
+user_input = st.text_input("Enter your query: "
+                           )
+
+#check condition user input not be blank
+if st.button("submit"):
+    if user_input.strip() == "":
+        st.warning("Please enter a prompt!")
+    else:
+        st.write("You:", user_input)
+        with st.spinner("Generating response....."):
+            result = model.invoke(user_input)
+            st.subheader("📄 Response:")
+            st.write(result.content)
+        
